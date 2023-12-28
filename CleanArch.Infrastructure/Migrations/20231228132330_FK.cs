4dd9a17b05c7f9308c400a50c18ee47e24e37ccf
Mohammedmassod <mohammedmassowd@gmail.com>
2023-12-28 16:46:04 +0300

    #Commin

---
+++ CleanArch.Infrastructure/Migrations/20231228132330_FK.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyProject.Infrastructure.Migrations
+{
+    public partial class FK : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "UserGroups",
+                newName: "UserGroupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserGroupId",
+                table: "UserGroups",
+                newName: "Id");
+        }
+    }
+}
